8aaec98888072bca9484de1dba01042478eb7219
sergio-miranda-usa <77934395+sergio-miranda-usa@users.noreply.github.com>
2024-02-04 11:37:53 -0500

    Add files via upload
    Lesson 1 - code for tank drive.  Forward and stop from gamepad controller.

---
+++ tanker.py
@@ -0,0 +1,78 @@
+import evdev
+from evdev import InputDevice, categorize
+import motoron
+
+def find_gamepadpath():
+    
+    #set return string
+    retString = ""
+    
+    #iterate through all input devices
+    devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+    
+    for device in devices:
+        #print all devices attached to the box
+        #print (device.name)
+        
+        #check if the device is a joystick
+        if "Gamepad" in device.name:
+            print(device.name)
+            retString = device.path
+    
+    return retString
+
+def capture_gamepad_keys(gamePadPath):
+    
+    #print the controller path
+    print(gamePadPath)
+    
+    #get device from path
+    gamePad = InputDevice(gamePadPath)
+    
+    #interrogate the pad for events
+    for event in gamePad.read_loop():
+        
+        print(categorize(event))
+        print(str(event.code) + " : " + str(event.value))
+                
+        #304 A
+        #308 Y
+        #305 B
+        #307 X
+        #310 LB
+        #311 RB
+        #2  LT
+        #5  RT
+        #16 DPad R L
+        #17 DPad Up Down
+        
+        #according to the button pressed, activate motor
+        if event.code == 308: #BTN_Y
+            if event.value == 1: #down
+                motor_forward()
+            elif event.value == 0: #up
+                motor_stop()
+
+                
+def motor_forward():
+    #move motors forward
+    mc.set_speed(1,200)
+    mc.set_speed(2,-200) #negative because it is facing opposite
+    
+def motor_stop():
+    #stop all motors to freewheel
+    mc.coast_now()
+    
+
+#Start Here:
+mc = motoron.MotoronI2C()
+mc.reinitialize()
+mc.disable_crc()
+mc.clear_reset_flag()
+mc.disable_command_timeout()
+
+#Get gamepad
+gamePadPath = find_gamepadpath()
+capture_gamepad_keys(gamePadPath)
+
+
